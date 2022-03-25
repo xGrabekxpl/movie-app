eaf07f1b365a8dae848877747b6d70deab57a6f9
xgrabekxpl <grabek660@gmail.com>
2022-03-25 22:36:10 +0100

    import styles, set up a project to start coding

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+const API_URL = "http://www.omdbapi.com/?apikey=4078af18";
 
 const App = () => {
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data);
+  }
+
+  useEffect(() => {
+    searchMovies("spiderman");
+  }, []);
+
   return (
     <div>
       This is my app
